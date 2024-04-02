a243c689711be357b96a554202cd60b1a575e8cd
Sai Kumar <kasarla@usc.edu>
2024-04-02 11:00:47 -0700

    changes

--- run.py
+++ run.py
@@ -21,7 +21,8 @@ with open(secret_key_file_path, "r") as file:
 secret_key = text_content
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(os.getcwd(), 'users.db')  # Using SQLite
+db_path = os.path.join(app.root_path, 'app/users.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_path  # Using SQLite
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = secret_key  # Secret key for JWT token generation
 CORS(app)
